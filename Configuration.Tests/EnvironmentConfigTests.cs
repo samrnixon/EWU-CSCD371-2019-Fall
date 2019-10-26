bb14ce57500956858e174ba91d98f2e3d2500687
Samuel Nixon <samnixon117@gmail.com>
2019-10-26 15:37:25 -0700

    WIP

---
+++ Configuration.Tests/EnvironmentConfigTests.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Configuration.Tests
+{
+    [TestClass]
+    public class EnvironmentConfigTests
+    {
+        [TestMethod]
+        public void GetConfigValue_ValueSentIsNullReturnTrue()
+        {
+            var environmentConfig = new EnvironmentConfig();
+            string name = "PATH";
+            string value = null;
+            bool testBool = environmentConfig.GetConfigValue(name, value);
+            Assert.IsTrue(testBool);
+        }
+        [TestMethod]
+        public void GetConfigValue_NameSentisNullReturnFalse()
+        {
+            var environmentConfig = new EnvironmentConfig();
+            string name = null;
+            string value = "VALUE";
+            bool testBool = environmentConfig.GetConfigValue(name, value);
+            Assert.IsFalse(testBool);
+        }
+
+        [TestMethod]
+        public void SetConfigValue_AllNormalReturnsTrue()
+        {
+            var environmentConfig = new EnvironmentConfig();
+            string name = "NAME";
+            string value = "VALUE";
+            bool testBool = environmentConfig.SetConfigValue(name, value);
+            Assert.IsTrue(testBool);
+        }
+    }
+}
---
+++ Configuration/EnvironmentConfig.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Configuration
+{
+    public class EnvironmentConfig : IConfig
+    {
+        private List<string> nameList;
+        public EnvironmentConfig()
+        {
+            nameList = new List<string>();
+        }
+        public bool GetConfigValue(string name, string? value)
+        {
+            if (name is null) { return false; }
+            value = Environment.GetEnvironmentVariable(name);
+            if(value is null)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        public bool SetConfigValue(string name, string? value)
+        {
+            if (value is null || name is null)
+            {
+                return false;
+            }
+            nameList.Add(name);
+            Environment.SetEnvironmentVariable(name, value);
+            return true;
+        }
+
+    }
+}

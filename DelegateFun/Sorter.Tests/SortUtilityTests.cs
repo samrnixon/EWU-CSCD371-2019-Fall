976e9ae08453cc1d81d3ff917155b3ad12dcf416
Samuel Nixon <samnixon117@gmail.com>
2019-11-15 14:58:19 -0800

    Assignment7 samrnixon

--- DelegateFun/Sorter.Tests/SortUtilityTests.cs
+++ DelegateFun/Sorter.Tests/SortUtilityTests.cs
@@ -1,13 +1,66 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using static Sorter.SortUtility;
 
 namespace Sorter.Tests
 {
     [TestClass]
     public class SortUtilityTests
     {
+
         [TestMethod]
-        public void SortUtility_ShouldSortAscending_UsingAnAnonymousMethod()
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void SortUtilNullArray_Fail()
         {
+            SortUtility sortUtility = new SortUtility();
+            sortUtility.SelectionSortUtil(null!, null!);
+        }
+
+        [TestMethod]
+        public void SortUtil_SortAscending_UsingAnAnonymousMethod()
+        {
+            int[] array = new int[] { 3, 34, 7, 45, 8, 99, 21 };
+            int[] expected = new int[] { 3, 7, 8, 21, 34, 45, 99 };
+
+            Analyze analyze = delegate (int x, int y)
+            {
+                return x < y;
+            };
+
+            SortUtility sortUtility = new SortUtility();
+
+            array = sortUtility.SelectionSortUtil(array, analyze);
+
+            CollectionAssert.AreEqual(array, expected);
+        }
+
+        [TestMethod]
+        public void SortUtil_SortDescending_UsingALambdaExpression()
+        {
+            int[] array = new int[] { 78, 4, 9, 77, 23, 100 };
+            int[] expected = new int[] { 100, 78, 77, 23, 9, 4 };
+
+            SortUtility sortUtility = new SortUtility();
+
+            array = sortUtility.SelectionSortUtil(array, ((int x, int y) => x > y));
+
+            CollectionAssert.AreEqual(array, expected);
+        }
+
+        [TestMethod]
+        public void SortUtil_SortAscending_UsingALambdaStatement()
+        {
+            int[] array = new int[] { 20, 1, 765, 888, 999, 234, 453 };
+            int[] expected = new int[] { 1, 20, 765, 888, 999, 234, 453 };
+
+            SortUtility sortUtility = new SortUtility();
+
+            array = sortUtility.SelectionSortUtil(array, (x, y) => 
+            {
+                return x < 50;
+            });
+
+            CollectionAssert.AreEqual(array, expected);
         }
     }
 }
--- DelegateFun/Sorter/SortUtility.cs
+++ DelegateFun/Sorter/SortUtility.cs
@@ -4,7 +4,32 @@ namespace Sorter
 {
     public class SortUtility
     {
-        // Sort method should be implemented here
-        // It should accept an int[] and a delegate you define that performs the actual comparison
+        public delegate bool Analyze(int x, int y);
+
+        public int[] SelectionSortUtil(int[] intArray, Analyze analyze)
+        {
+            if (intArray is null || analyze is null)
+            {
+                throw new ArgumentNullException(nameof(intArray));
+            }
+
+            for (int start = 0; start < intArray.Length - 1; start++)
+            {
+                int min = start;
+                for (int search = start + 1; search < intArray.Length; search++)
+                {
+                    if(analyze(intArray[search],intArray[min]))
+                    {
+                        min = search;
+                    }
+                }
+                int temp = intArray[min];
+                intArray[min] = intArray[start];
+                intArray[start] = temp;
+            }
+
+            return intArray;
+        }
+
     }
 }
